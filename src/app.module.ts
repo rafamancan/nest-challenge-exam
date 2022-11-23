8da7b6f7a5431c4a89459bc3235ea2caeb09e5e4
Rafael Mançan <rafael.mancan@gmail.com>
2022-11-23 20:45:21 -0300

    feat(app): add auth and user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './modules/prisma';
 import { ExamModule } from './modules/exams/exams.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [PrismaModule, ExamModule],
+  imports: [PrismaModule, ExamModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
