7dec5e91ad95c38eb4708fd4c83e8d8aab10c2db
Rafael Mançan <rafael.mancan@gmail.com>
2022-11-23 20:45:48 -0300

    feat(modules): exams - add guards to secure requests from only logged users

--- src/modules/exams/exams.controller.ts
+++ src/modules/exams/exams.controller.ts
@@ -4,11 +4,15 @@ import {
   Controller,
   Post,
   Get,
+  UseGuards,
 } from '@nestjs/common';
 import { yupCreateExamInput } from '../../yup/exams';
 import { ExamService } from './exams.service';
 import { CreateExamInput } from './dto/exam';
+import { LocalAuthGuard } from '../auth/local-auth.guard';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller()
 export class ExamController {
   constructor(private readonly examService: ExamService) {}
