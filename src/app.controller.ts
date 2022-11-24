7a101d58ceb11253d0b7023032dc22d2489a4f06
Rafael Mançan <rafael.mancan@gmail.com>
2022-11-23 21:17:31 -0300

    fix(deploy): again rs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request, Get } from '@nestjs/common';
 import { LocalAuthGuard } from './modules/auth/local-auth.guard';
 import { AuthService } from './modules/auth/auth.service';
 
 @Controller()
 export class AppController {
   constructor(private authService: AuthService) {}
+
+  @Get('')
+  async welcome() {
+    return 'Welcome to the NestJS Challenge Exam 😁';
+  }
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
